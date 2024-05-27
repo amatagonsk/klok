e6cbf8ceaac1c1183f1f0d6d0ea86891a619009e
default <dummy@ma.il>
2024-05-27 17:01:22 +0900

    feat: simple clock (with many warning)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use chrono::{DateTime, Local, Utc};
+use chrono::{DateTime, Datelike, Local, Utc};
 use color_eyre::{
     eyre::{bail, WrapErr},
     Result,
 };
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    terminal::size,
+};
 use ratatui::{
     layout::Offset,
     prelude::*,
@@ -28,6 +31,8 @@ fn main() -> Result<()> {
 
 #[derive(Debug, Default)]
 pub struct App {
+    year_month_day: String,
+    weekday: String,
     time: String,
     exit: bool,
 }
@@ -43,44 +48,69 @@ impl App {
         Ok(())
     }
 
-    fn centered_rect(r: Rect, percent_x: u16, percent_y: u16) -> Rect {
-        let clock_height = 10;
+    fn centered_rect(r: Rect) -> Rect {
+        // full size 8x8
+        let clock_height = 8 + 1;
+        let clock_width = (8 * 8) + 2;
         let popup_layout = Layout::default()
             .direction(Direction::Vertical)
             .constraints([
-                Constraint::Percentage((100 - percent_y) / 2),
+                Constraint::Percentage((100 - clock_height) / 2),
                 Constraint::Min(clock_height),
                 Constraint::Max(clock_height),
-                Constraint::Percentage((100 - percent_y) / 2),
+                Constraint::Percentage((100 - clock_height) / 2),
             ])
             .split(r);
 
         Layout::default()
             .direction(Direction::Horizontal)
             .constraints([
-                Constraint::Percentage((100 - percent_x) / 2),
-                Constraint::Percentage(percent_x),
-                Constraint::Percentage((100 - percent_x) / 2),
+                Constraint::Percentage((100 - clock_width) / 2),
+                Constraint::Min(clock_width),
+                Constraint::Max(clock_width),
+                Constraint::Percentage((100 - clock_width) / 2),
             ])
             .split(popup_layout[1])[1]
     }
 
     fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(
-            self,
-            // frame.size(), // .offset(Offset {
-                Self::centered_rect(frame.size(), 100, 30)
-                        //       x: 0,
-                        //       // tui-big-text full 8x8
-                        //       y: (((frame.size().height / 2) - (8 / 2)) as i32),
-                        //   }),
-        );
+        // frame.render_widget(
+        //     self,
+        //     // frame.size(), // .offset(Offset {
+        //     Self::centered_rect(frame.size(), 100, 30), //       x: 0,
+        //                                                 //       // tui-big-text full 8x8
+        //                                                 //       y: (((frame.size().height / 2) - (8 / 2)) as i32),
+        //                                                 //   }),
+        // );
+        let ymd = <String as Clone>::clone(&self.year_month_day);
+        let weekday = <String as Clone>::clone(&self.weekday);
+        let block = Block::bordered()
+            .border_type(BorderType::Rounded)
+            .title(format!(" {} {} ", ymd, weekday))
+            .title_bottom(Line::from(" exit: <q> or <Esc> ").centered());
+
+        // let center_frame= App::centered_rect(frame.size());
+        let center_frame = App::centered_rect(frame.size());
+        // let area = block.inner(frame.size());
+        frame.render_widget(&block, center_frame);
+
+        let big_text = BigText::builder()
+            .style(Style::new())
+            .pixel_size(PixelSize::Full)
+            .lines(vec![<String as Clone>::clone(&self.time).into()])
+            .build()
+            .unwrap();
+        frame.render_widget(big_text, block.inner(center_frame));
     }
 
     fn tictac(&mut self) {
         let local_date_time: DateTime<Local> = Local::now();
-        let local_formatted = format!("{}", local_date_time.format("%H:%M:%S"));
-        self.time = local_formatted;
+        let local_time_formatted = format!("{}", local_date_time.format("%H:%M:%S"));
+        let local_ymd_formatted = format!("{}", local_date_time.format("%Y-%m-%d"));
+        let local_weekday = format!("{}", local_date_time.weekday());
+        self.time = local_time_formatted;
+        self.year_month_day = local_ymd_formatted;
+        self.weekday = local_weekday;
     }
 
     /// updates the application's state based on user input
@@ -110,30 +140,30 @@ impl App {
     }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" clock ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Quit:".into(),
-            " <Q> ".blue().bold(),
-            "or".bold(),
-            " <Esc> ".blue().bold(),
-        ]));
-        let block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .borders(Borders::ALL)
-            .border_type(BorderType::Rounded);
+// impl Widget for &App {
+//     fn render(self, area: Rect, buf: &mut Buffer) {
+//         let title = Title::from(" clock ".bold());
+//         let instructions = Title::from(Line::from(vec![
+//             " Quit:".into(),
+//             " <Q> ".blue().bold(),
+//             "or".bold(),
+//             " <Esc> ".blue().bold(),
+//         ]));
+//         let block = Block::default()
+//             .title(title.alignment(Alignment::Center))
+//             .title(
+//                 instructions
+//                     .alignment(Alignment::Center)
+//                     .position(Position::Bottom),
+//             )
+//             .borders(Borders::ALL)
+//             .border_type(BorderType::Rounded);
 
-        let clock = self.time.to_string();
+//         let clock = self.time.to_string();
 
-        Paragraph::new(clock)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
-}
+//         Paragraph::new(clock)
+//             .centered()
+//             .block(block)
+//             .render(area, buf);
+//     }
+// }
